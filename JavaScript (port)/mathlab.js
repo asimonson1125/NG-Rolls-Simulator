10a2ce945ef0fc8652337ef2ff0aed6f8f2da5ab
asimonson1125 <asimonson1125@users.noreply.github.com>
2020-06-14 22:44:32 -0400

    fixed SF counter

--- JavaScript (port)/mathlab.js
+++ JavaScript (port)/mathlab.js
@@ -27,23 +27,29 @@ exports.attack = function(firer, target, targetAllies){
     let man,fp,tArm,tMan;
     firer.typeBonuses.forEach(function(i){if(i[0] == target.type){targetBonus = i[1];}});
     let multiplier = (firer.bonuses + targetBonus);    
-    
-    if (firer.counters.includes(target.type)){
-        fp = (1.25 * firer.firepower + 6);
-        man = (1.25 * firer.maneuver + 3);
-    }
-    else{
+    let counters = false;
+    firer.counters.forEach(function(x){
+        if (x.toLowerCase() == target.type.toLowerCase()){
+            fp = (1.25 * firer.firepower + 6);
+            man = (1.25 * firer.maneuver + 3);
+        }
+    });
+    if(counters == false){
         fp = firer.firepower;
         man = firer.maneuver;
     }
     targetBonus = 0;
     target.typeBonuses.forEach(function(i){if(i[0] == firer.type){ targetBonus = i[1];}});
-    let tMultiplier = (target.bonuses + targetBonus);    
-    if (target.counters.includes(firer.type)){
-        tArm = (1.25 * target.armor + 3);
-        tMan = (1.25 * target.maneuver + 3);
-    }
-    else{
+    let tMultiplier = (target.bonuses + targetBonus);
+    counters = false;
+    target.counters.forEach(function(x){
+        if (x.toLowerCase() == firer.type.toLowerCase()){
+            counters = true;
+            tArm = (1.25 * target.armor + 3);
+            tMan = (1.25 * target.maneuver + 3);
+        }
+    });
+    if(counters == false){
         tArm = target.armor;
         tMan = target.maneuver;
     }
@@ -56,11 +62,17 @@ exports.attack = function(firer, target, targetAllies){
     let damageReduct = 0
     targetAllies.forEach(function(i){i.healing.forEach(function(type){if(type[0] == target.type){ damageReduct += type[1];}});});
     if(damageReduct > 30){ damageReduct = 30;} // 30% is max friendly damage reduction
-    if (firer.type in target.counters){ healing = Math.round(damage * ((damageReduct + 50) / 100));}
-    else{ healing = Math.round(damage * (damageReduct / 100));}
+    counters = false;
+    target.counters.forEach(function(x){
+        if(x.toLowerCase() == firer.type.toLowerCase() && counters == false){
+            healing = Math.round(damage * ((damageReduct + 50) / 100));
+            counters = true;
+        }
+    });
+    if(counters == false){ healing = Math.round(damage * (damageReduct / 100));}
     damage = Math.ceil(damage - healing);
     // damage cannot be 0 or negative, so this is my compensation since we don't know how the real calculation works
-    if(damage < 5){ damage = 5;}
+    if(damage < 3){ damage = 3;}
     target.hp -= damage;
     if (target.hp <= 0){ target.alive = false;}
     return;
@@ -77,7 +89,9 @@ exports.initiativeRoll = function(friendlies, enemies){
     unitList.forEach(function(x){
         let iRoll = Math.floor(Math.random() * 100) + 1;
         x[1] = iRoll * Math.pow((x[0].hp / x[0].maxhp),2);  // hp factor squared for more dramatic effect
-        if("Special Forces" in x[0].counters){ x[1] *= 3;}
+        x[0].counters.forEach(function(i){
+        if("special forces" == i.toLowerCase()){ x[1] *= 3;}
+        });
     });
     // Now each unit is assigned an initiative roll.  Now to sort them.
     unitList.sort(function(a, b){return b[1]-a[1]});
--- JavaScript (port)/predict.js
+++ JavaScript (port)/predict.js
@@ -12,46 +12,33 @@ exampleMortar = new unitClass.unit("Infantry", 3, 2, 2, 50, 20, [["Armor", 10],[
 exampleInfantry = new unitClass.unit("Infantry", 3, 1, 2, 50, 0, [], [], [],1);
 testHealer = new unitClass.unit("idkMan", 0,0,0,10,0,[],[],[["Infantry",30]],1);
 
-unit1 = new unitClass.unit("Armor",11,17,13,234,60,[["Armor",40], ["Static",40]],[],[["Air",20]],1);
-unit2 = new unitClass.unit("Armor",14,15,10,260,40,[["Infantry",20], ["Armor",20]],[],[["Armor",20], ["Special Forces",20], ["Air",40]],1);
-unit3 = new unitClass.unit("Infantry",13,12,8,128,40,[],[],[["Air",20], ["Static",20], ["Special Forces",20], ["Infantry",20]],1);
-unit4 = new unitClass.unit("Infantry",8,19,10,104,40,[["Static",20]],[],[["Static",20]],1);
-unit5 = new unitClass.unit("Infantry",12,15,10,104,80,[["Special forces",20], ["Infantry",20]],[],[["Infantry",20], ["Static",20]],1);
-unit6 = new unitClass.unit("Infantry",8,14,17,80,40,[["Special forces",20]],["Armor"],[["Air",40], ["Static",20]],1);
-unit7 = new unitClass.unit("Infantry",17,8,12,104,80,[["Infantry",40]],[],[["Special Forces",20]],1);
-unit8 = new unitClass.unit("Infantry",14,10,13,92,20,[["Armor",40], ["Special forces",20]],["Armor"],[["Special Forces",40], ["Armor",20], ["Air",20]],1);
-unit9 = new unitClass.unit("Infantry",12,14,10,110,0,[["Static",20], ["Air",40]],[],[["Air",20], ["Static",20], ["Special Forces",20], ["Armor",20]],1);
-unit10 = new unitClass.unit("Infantry",9,12,12,116,20,[["Static",20], ["Air",20], ["Special forces",20], ["Infantry",20]],["Armor"],[["Infantry",20], ["Static",20]],1);
-unit11 = new unitClass.unit("Infantry",13,10,11,110,80,[],["Armor"],[["Static",40], ["Special Forces",20], ["Armor",20]],1);
-unit12 = new unitClass.unit("Infantry",11,9,13,128,60,[],[],[["Static",20], ["Infantry",40], ["Special Forces",20]],1);
-unit13 = new unitClass.unit("Infantry",13,15,13,80,80,[],[],[["Air",40]],1);
-unit14 = new unitClass.unit("Infantry",15,11,12,98,20,[["Infantry",20], ["Special forces",20]],[],[["Static",20], ["Armor",20], ["Infantry",40]],1);
-unit15 = new unitClass.unit("Infantry",12,8,13,128,20,[["Special forces",20], ["Air",20]],[],[["Armor",20], ["Special Forces",40]],1);
-unit16 = new unitClass.unit("Infantry",15,13,12,74,40,[["Static",20], ["Infantry",20]],["Armor"],[["Static",20], ["Air",20]],1);
-unit17 = new unitClass.unit("Infantry",11,12,12,104,40,[["Air",20], ["Armor",40]],["Armor"],[["Static",20], ["Armor",20]],1);
-unit18 = new unitClass.unit("Infantry",12,7,16,116,20,[["Static",20], ["Air",20]],[],[["Air",20], ["Infantry",20]],1);
-unit19 = new unitClass.unit("Infantry",12,13,13,98,80,[["Static",20], ["Air",20]],[],[["Armor",20], ["Infantry",20]],1);
-unit20 = new unitClass.unit("Infantry",7,14,15,110,40,[["Static",40], ["Armor",20]],[],[["Armor",20], ["Air",20]],1);
+unit1 = new unitClass.unit("Special Forces",4,1,4,50,0,[["Infantry",50]],[],[],1);
+unit2 = new unitClass.unit("Special Forces",4,1,4,50,0,[["Infantry",50]],[],[],1);
+unit3 = new unitClass.unit("Special Forces",4,1,4,50,0,[["Infantry",50]],[],[],1);
+unit4 = new unitClass.unit("Special Forces",4,1,4,50,0,[["Infantry",50]],[],[],1);
+unit5 = new unitClass.unit("Special Forces",4,1,4,50,0,[["Infantry",50]],[],[],1);
+unit6 = new unitClass.unit("Special Forces",4,1,4,50,0,[["Infantry",50]],[],[],1);
+unit7 = new unitClass.unit("Special Forces",4,1,4,50,0,[["Infantry",50]],[],[],1);
+unit8 = new unitClass.unit("Special Forces",4,1,4,50,0,[["Infantry",50]],[],[],1);
+unit9 = new unitClass.unit("Special Forces",4,1,4,50,0,[["Infantry",50]],[],[],1);
 
-eunit1 = new unitClass.unit("Air",14,8,16,182,0,[["Infantry",30], ["Static",20]],["Infantry", "Armor"],[["Special Forces",10]],1);
-eunit2 = new unitClass.unit("Air",18,12,9,189,0,[["Infantry",30]],["Infantry", "Armor"],[["Armor",10]],1);
-eunit3 = new unitClass.unit("Air",22,12,11,140,0,[["Special forces",10], ["Infantry",50]],["Infantry", "Armor"],[],1);
-eunit4 = new unitClass.unit("Air",23,8,17,140,0,[["Infantry",30]],["Infantry", "Armor"],[],1);
-eunit5 = new unitClass.unit("Air",20,8,15,147,0,[["Infantry",40]],["Infantry", "Armor"],[["Special Forces",10]],1);
-eunit6 = new unitClass.unit("Air",19,10,10,161,0,[["Infantry",50], ["Static",20]],["Infantry", "Armor"],[],1);
-eunit7 = new unitClass.unit("Air",21,11,13,147,10,[["Infantry",30]],["Infantry", "Armor"],[["Armor",10]],1);
-eunit8 = new unitClass.unit("Air",17,10,13,182,0,[["Infantry",30]],["Infantry", "Armor"],[["Air",10]],1);
-eunit9 = new unitClass.unit("Air",21,10,11,126,0,[["Infantry",40]],["Infantry", "Armor"],[["Static",10], ["Special Forces",10], ["Air",30], ["Armor",10]],1);
-eunit10 = new unitClass.unit("Air",15,14,14,147,0,[["Infantry",40], ["Special forces",10]],["Infantry", "Armor"],[],1);
-
+eunit1 = new unitClass.unit("Infantry",5,2,4,56,0,[],["Special Forces"],[],1);
+eunit2 = new unitClass.unit("Infantry",4,4,2,60,0,[],["Special Forces"],[],1);
+eunit3 = new unitClass.unit("Infantry",5,2,2,62,0,[],["Special Forces"],[],1);
+eunit4 = new unitClass.unit("Infantry",4,1,4,62,0,[],["Special Forces"],[],1);
+eunit5 = new unitClass.unit("Infantry",4,2,5,50,0,[["Static",10]],["Special Forces"],[],1);
+eunit6 = new unitClass.unit("Infantry",3,2,5,62,0,[],["Special Forces"],[],1);
+eunit7 = new unitClass.unit("Infantry",3,2,2,50,0,[],["Special Forces"],[],1);
+eunit8 = new unitClass.unit("Infantry",3,1,3,56,0,[],["Special Forces"],[],1);
+eunit9 = new unitClass.unit("Infantry",3,1,2,56,0,[],["Special Forces"],[],1);
 
 // ______________________________________________________________________________
 
 // instantBattle simulates the battle 'rounds' number of times and prints how many each side won with remaining units
 // change pFriendlies and pEnemies to fit how many units you have in your battle and what you named them.
 async function instantBattle(rounds){
-    const pFriendlies = [unit1, unit2, unit3, unit4, unit5, unit6, unit7, unit8, unit9, unit10, unit11, unit12, unit13, unit14, unit15, unit16, unit17, unit18, unit19, unit20];
-    const pEnemies = [eunit1, eunit2, eunit3, eunit4, eunit5, eunit6, eunit7, eunit8, eunit9, eunit10];//, eunit11, eunit12, eunit13, eunit14, eunit15, eunit16, eunit17, eunit18, eunit19, eunit20];
+    const pFriendlies = [unit1, unit2, unit3, unit4, unit5, unit6, unit7, unit8, unit9]//, unit10, unit11, unit12, unit13, unit14, unit15, unit16, unit17, unit18, unit19, unit20];
+    const pEnemies = [eunit1, eunit2, eunit3, eunit4, eunit5, eunit6, eunit7, eunit8, eunit9]//, eunit10, eunit11, eunit12, eunit13, eunit14, eunit15, eunit16, eunit17, eunit18, eunit19, eunit20];
 
     let friendlies = pFriendlies.slice();
     let enemies = pEnemies.slice();
@@ -119,4 +106,5 @@ async function instantBattle(rounds){
 instantBattle(1000);
 
 //mathlab.attack(attacker,defender,allies);
+//mathlab.attack(eunit1,unit1,[]);
 //attack(exampleMortar, exampleInfantry, [exampleInfantry, testHealer]);
